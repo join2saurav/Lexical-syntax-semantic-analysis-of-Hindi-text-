76d6bd622d2f9178faf64e80303bfcf9ed89961f
Saurabh Shrama <join2saurav3@gmail.com>
2018-04-22 00:30:54 +0530

    Delete test14.py

--- test14.py
+++
@@ -1,8 +0,0 @@
-import collections
-g=open("depth_32.txt","w")
-with open('depth_21.txt') as infile:
-    counts = collections.Counter(l.strip() for l in infile)
-for line, count in counts.most_common():
-    g.write(str(line))
-    #g.write(str(count))
-    g.write("\n")
